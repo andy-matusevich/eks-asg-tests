5d50e1c2cc16c3b1b78860e42222e3cf49145a7f
Andrei Matusevich <andy.matusevich@gmail.com>
2020-06-08 19:34:32 +0300

    change tf folder in ci jobs

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -1,3 +1,11 @@
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+  version                = "~> 1.11"
+}
+
 module "eks" {
   source       = "terraform-aws-modules/eks/aws"
   cluster_name = local.cluster_name
