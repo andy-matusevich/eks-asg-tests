9c1d22b734d1ad1b1cc7bbf3ca17e552b9d1340a
Andrei Matusevich <andy.matusevich@gmail.com>
2020-06-09 20:42:15 +0300

    change pre jobs

--- terraform/pre/s3.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_s3_bucket" "state-file-bucket" {
-  bucket = "eks-asg-tests"
-
-  versioning {
-    enabled = true
-  }
-
-  tags = {}
-}
---
+++ terraform/pre/tfstate.tf
@@ -0,0 +1,26 @@
+provider "aws" {
+    region = var.tfstate_region
+}
+
+resource "aws_s3_bucket" "tfstate-storage-s3" {
+    bucket = var.tfstate_bucket
+
+    versioning {
+      enabled = true
+    }
+
+    lifecycle {
+      prevent_destroy = true
+    }
+
+}
+
+resource "aws_dynamodb_table" "tfstate-locks" {
+  name         = var.tfstate_lock_table
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
