485ce1197ceca3ca9d8fce8b3dbe01cb0042d6cd
Andrei Matusevich <andy.matusevich@gmail.com>
2020-06-19 16:40:19 +0300

    workaround for aws-cli orb (sudo dependency failed)

--- .circleci/aws-cli-install.sh
+++ .circleci/aws-cli-install.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -leo pipefail
 
-apk add --no-cache --update .build-deps g++ python3-dev libffi-dev openssl-dev sudo
-apk update && apk add curl
+apk update
+apk add --no-cache --update g++ python3-dev libffi-dev openssl-dev sudo
 echo "Set disable_coredump false" >> /etc/sudo.conf
-apk add --no-cache --update python3
+apk add --no-cache --update curl python3
 pip3 install --upgrade pip setuptools
 pip3 install awscli --upgrade
 
