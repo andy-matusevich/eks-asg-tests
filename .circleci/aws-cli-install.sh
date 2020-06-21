6cff7fc5437a2279d23e1093d0f0bc39a1779264
Andrei Matusevich <andy.matusevich@gmail.com>
2020-06-21 21:02:30 +0300

    minor fixes

--- .circleci/aws-cli-install.sh
+++ .circleci/aws-cli-install.sh
@@ -3,7 +3,7 @@
 apk update
 apk add --no-cache --update g++ python3-dev libffi-dev openssl openssl-dev sudo
 echo "Set disable_coredump false" >> /etc/sudo.conf
-apk add --no-cache --update curl python3 bash
+apk add --no-cache --update curl python3 bash jq
 pip3 install --upgrade pip setuptools
 pip3 install --upgrade awscli
 
