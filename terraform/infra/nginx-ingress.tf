6531849f292428412fddd3eabad99929c514cc4a
Andrei Matusevich <andy.matusevich@gmail.com>
2020-06-21 16:25:19 +0300

    minor fixes

--- terraform/infra/nginx-ingress.tf
+++ terraform/infra/nginx-ingress.tf
@@ -1,3 +1,4 @@
+# https://www.terraform.io/docs/providers/helm/index.html
 provider "helm" {
   kubernetes {
     host                   = data.aws_eks_cluster.cluster.endpoint
@@ -7,14 +8,15 @@ provider "helm" {
 }
 
 ### nginx-ingress
+# https://www.terraform.io/docs/providers/helm/r/release.html
 resource "helm_release" "nginx-ingress" {
-  name  = "nginx"
-  chart = "nginx-stable/nginx-ingress"
-  repository = "https://helm.nginx.com/stable"
-  namespace = "ingress"
-  replace = "true"
+  name             = "nginx"
+  chart            = "nginx-ingress"
+  repository       = "https://helm.nginx.com/stable"
+  namespace        = "ingress"
+  replace          = "true"
   create_namespace = "true"
-
+  atomic           = "true"
 
   set {
     name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-type"
@@ -37,5 +39,13 @@ resource "helm_release" "nginx-ingress" {
     name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-cross-zone-load-balancing-enabled"
     value = "true"
   }
+
+  # /nginx-health
+  set {
+    type  = "string"
+    name  = "controller.healthStatus"
+    value = "true"
+  }
+
 
 }
