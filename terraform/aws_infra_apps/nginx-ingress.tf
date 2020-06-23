0fd0024183485a0242544d79802892235e02b0ab
Andrei Matusevich <andy.matusevich@gmail.com>
2020-06-23 16:21:41 +0300

    change ingress controller (nginxinc-->k8s)

--- terraform/aws_infra_apps/nginx-ingress.tf
+++ terraform/aws_infra_apps/nginx-ingress.tf
@@ -3,39 +3,33 @@
 resource "helm_release" "nginx-ingress" {
   name             = "nginx"
   chart            = "nginx-ingress"
-  repository       = "https://helm.nginx.com/stable"
+  repository       = "https://kubernetes.github.io/ingress-nginx"
   namespace        = "ingress"
   replace          = "true"
   create_namespace = "true"
   atomic           = "true"
   lint             = "true"
 
+
   set {
-    name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-type"
+    name  = "nginx.ingress.kubernetes.io.service\\.beta\\.kubernetes\\.io/aws-load-balancer-type"
     value = "nlb"
   }
 
   set {
-    name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-backend-protocol"
+    name  = "nginx.ingress.kubernetes.io.service\\.beta\\.kubernetes\\.io/aws-load-balancer-backend-protocol"
     value = "tcp"
   }
 
   set {
     type  = "string"
-    name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-connection-idle-timeout"
+    name  = "nginx.ingress.kubernetes.io.service\\.beta\\.kubernetes\\.io/aws-load-balancer-connection-idle-timeout"
     value = "60"
   }
 
   set {
     type  = "string"
-    name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-cross-zone-load-balancing-enabled"
-    value = "true"
-  }
-
-  # /nginx-health
-  set {
-    type  = "string"
-    name  = "controller.healthStatus"
+    name  = "nginx.ingress.kubernetes.io.service\\.beta\\.kubernetes\\.io/aws-load-balancer-cross-zone-load-balancing-enabled"
     value = "true"
   }
 
