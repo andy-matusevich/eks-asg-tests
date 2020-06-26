d33a96315d1209eede2cd2f1f6453d4f3583f088
Andrei Matusevich <andy.matusevich@gmail.com>
2020-06-26 11:55:08 +0300

    add ecr resource

---
+++ terraform/aws_infra/ecr.tf
@@ -0,0 +1,9 @@
+resource "aws_ecr_repository" "ecr" {
+  name                 = var.cluster_name
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = "false"
+  }
+
+}
