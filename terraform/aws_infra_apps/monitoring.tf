44f746cea9e776053acc5aeff67644e185b727de
Andrei Matusevich <andy.matusevich@gmail.com>
2020-07-14 16:14:35 +0300

    test grafana ingress values

--- terraform/aws_infra_apps/monitoring.tf
+++ terraform/aws_infra_apps/monitoring.tf
@@ -1,38 +1,34 @@
+locals {
+  # common vars
+  kubernetes_node_assignment = "monitoring"
+  # prometheus
+  prometheus_name            = "prometheus"
+  prometheus_repository      = "https://kubernetes-charts.storage.googleapis.com/"
+  prometheus_chart_version   = "11.6.0"
+  prometheus_service_port    = "9090"
+  # loki
+  loki_name                  = "loki"
+  loki_version               = "0.38.1"
+  loki_repository            = "https://grafana.github.io/loki/charts"
+  # grafana
+  grafana_name               = "grafana"
+  grafana_repository         = "https://kubernetes-charts.storage.googleapis.com/"
+  grafana_chart_version      = "5.2.1"
+  grafana_service_port       = "3000"
+}
+
+# prometheus
 resource "helm_release" "prometheus_release" {
   depends_on       = [helm_release.ingress-nginx-controller]
-  name             = "prometheus"
-  chart            = "prometheus"
-  version          = "11.6.0"
-  repository       = "https://kubernetes-charts.storage.googleapis.com/"
-  namespace        = "monitoring"
+  name             = local.prometheus_name
+  chart            = local.prometheus_name
+  version          = local.prometheus_chart_version
+  repository       = local.prometheus_repository
+  namespace        = local.kubernetes_node_assignment
   replace          = "false"
   create_namespace = "true"
   lint             = "true"
-
-  set {
-    name  = "alertmanager\\.persistentVolume\\.storageClass"
-    value = "gp2"
-  }
-
-  set {
-    name  = "server\\.persistentVolume\\.storageClass"
-    value = "gp2"
-  }
-
-  set {
-    name  = "server\\.nodeSelector"
-    value = "node.kubernetes.io/assignment=monitoring"
-  }
-
-  set {
-    name  = "pushgateway\\.nodeSelector"
-    value = "node.kubernetes.io/assignment=monitoring"
-  }
-
-  set {
-    name  = "alertmanager\\.nodeSelector"
-    value = "node.kubernetes.io/assignment=monitoring"
-  }
+  values           = ["${file("values/prometheus.yaml")}"]
 }
 
 resource "kubernetes_ingress" "prometheus" {
@@ -40,15 +36,15 @@ resource "kubernetes_ingress" "prometheus" {
 
   spec {
     backend {
-      service_name = "prometheus-server"
-      service_port = "9090"
+      service_name = local.prometheus_name
+      service_port = local.prometheus_service_port
     }
     rule {
       http {
         path {
           backend {
-            service_name = "prometheus-server"
-            service_port = "9090"
+            service_name = local.prometheus_name
+            service_port = local.prometheus_service_port
           }
           path = "/"
         }
@@ -56,29 +52,26 @@ resource "kubernetes_ingress" "prometheus" {
     }
   }
   metadata {
-    name      = "ingress-prometheus"
-    namespace = "monitoring"
+    name      = local.prometheus_name
+    namespace = local.kubernetes_node_assignment
   }
 }
 
+# loki
 resource "helm_release" "loki_release" {
   depends_on       = [helm_release.ingress-nginx-controller]
-  name             = "loki"
-  chart            = "loki-stack"
-  version          = "0.38.1"
-  repository       = "https://grafana.github.io/loki/charts"
-  namespace        = "monitoring"
+  name             = local.loki_name
+  chart            = "${local.loki_name}-stack"
+  version          = local.loki_version
+  repository       = local.loki_repository
+  namespace        = local.kubernetes_node_assignment
   replace          = "false"
   create_namespace = "true"
   lint             = "true"
-
-  set {
-    name  = "nodeSelector"
-    value = "node.kubernetes.io/assignment=monitoring"
-  }
-
+  values           = ["${file("values/loki.yaml")}"]
 }
 
+# grafana
 resource "random_string" "random" {
   length           = 16
   special          = true
@@ -87,58 +80,18 @@ resource "random_string" "random" {
 
 resource "helm_release" "grafana_release" {
   depends_on       = [helm_release.prometheus_release, helm_release.loki_release]
-  name             = "grafana"
-  chart            = "grafana"
-  version          = "5.2.1"
-  repository       = "https://kubernetes-charts.storage.googleapis.com/"
-  namespace        = "monitoring"
+  name             = local.grafana_name
+  chart            = local.grafana_name
+  version          = local.grafana_chart_version
+  repository       = local.grafana_repository
+  namespace        = local.kubernetes_node_assignment
   replace          = "false"
   create_namespace = "true"
   lint             = "true"
   values           = ["${file("values/grafana.yaml")}"]
 
   set {
-    name  = "persistence\\.storageClassName"
-    value = "gp2"
-  }
-
-  set {
-    name  = "persistence\\.enabled"
-    value = "true"
-  }
-
-  set {
     name  = "adminPassword"
     value = random_string.random.result
   }
 }
-
-resource "kubernetes_ingress" "grafana" {
-  depends_on = [helm_release.grafana_release]
-
-  spec {
-    backend {
-      service_name = "grafana"
-      service_port = "3000"
-    }
-    rule {
-      http {
-        path {
-          backend {
-            service_name = "grafana"
-            service_port = "3000"
-          }
-          path = "/grafana(/|$)(.*)"
-        }
-      }
-    }
-  }
-  metadata {
-    name        = "ingress-grafana"
-    namespace   = "monitoring"
-    annotations = {
-      "nginx.ingress.kubernetes.io/rewrite-target" = "/$2"
-    }
-  }
-}
-
