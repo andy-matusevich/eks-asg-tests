3280a4d827219457202c94697d11bbc35cdd1cf9
Andrei Matusevich <andy.matusevich@gmail.com>
2020-10-15 23:46:01 +0300

    Downgrade EKS version to 1.15 (apiVersion issue)

--- terraform/aws_infra/eks-cluster.tf
+++ terraform/aws_infra/eks-cluster.tf
@@ -22,7 +22,7 @@ locals {
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   cluster_name    = var.cluster_name
-  cluster_version = "1.17"
+  cluster_version = "1.15"
   subnets         = module.vpc.private_subnets
 
   tags = {
