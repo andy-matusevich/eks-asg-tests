539de1f5f73e6e8154ec512cfed0613b552dadd3
Andrei Matusevich <andy.matusevich@gmail.com>
2022-02-16 14:27:01 +0300

    Check S3 resource

--- terraform/aws_state/tfstate.tf
+++ terraform/aws_state/tfstate.tf
@@ -9,20 +9,8 @@ variable "state_lock_table" {}
 resource "aws_s3_bucket" "tfstate-storage-s3" {
   bucket = var.state_bucket
 
-  versioning {
-    enabled = true
-  }
-
   lifecycle {
     prevent_destroy = true
-  }
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
   }
 
 }
