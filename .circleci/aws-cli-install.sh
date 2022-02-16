7cece5967082f9fd209494dcb707ed65f4194485
Andrei Matusevich <andy.matusevich@gmail.com>
2022-02-16 14:23:57 +0300

    Fix pip dependency

--- .circleci/aws-cli-install.sh
+++ .circleci/aws-cli-install.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -leo pipefail
 
 apk update
-apk add --no-cache --update g++ python3-dev libffi-dev openssl openssl-dev sudo
+apk add --no-cache --update g++ python3-dev pip3 libffi-dev openssl openssl-dev sudo
 echo "Set disable_coredump false" >> /etc/sudo.conf
 apk add --no-cache --update curl python3 bash jq
-pip3 install --upgrade pip3 setuptools
+pip3 install --upgrade pip setuptools
 pip3 install --upgrade awscli
 
